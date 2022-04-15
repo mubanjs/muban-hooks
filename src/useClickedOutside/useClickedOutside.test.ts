41d9428453fbb691abcb996814f9e2bc7f7a8ff8
Lars van Braam <lars@mediamonks.com>
2022-04-15 10:46:42 +0200

    Add the useClickedOutside hook

---
+++ src/useClickedOutside/useClickedOutside.test.ts
@@ -0,0 +1,120 @@
+import { createMockComponentRef, createMockElementRef, runComponentSetup } from '@muban/test-utils';
+import { useClickedOutside } from './useClickedOutside';
+
+jest.mock('@muban/muban', () => jest.requireActual('@muban/test-utils').getMubanLifecycleMock());
+
+describe('useEventListener', () => {
+  it('should not crash', async () => {
+    const target = document.createElement('div');
+    const mockHandler = jest.fn();
+
+    await runComponentSetup(() => {
+      useClickedOutside(target, mockHandler);
+    });
+  });
+
+  it('should call addEventListener on the target once', async () => {
+    const target = document.createElement('div');
+    const listenerSpy = jest.spyOn(document, 'addEventListener');
+
+    await runComponentSetup(() => {
+      useClickedOutside(target, () => undefined);
+    });
+
+    expect(listenerSpy).toBeCalledTimes(1);
+  });
+
+  it('should call removeEventListener on the target when unmounted', async () => {
+    const target = document.createElement('div');
+    const listenerSpy = jest.spyOn(document, 'removeEventListener');
+
+    await runComponentSetup(() => {
+      useClickedOutside(target, () => undefined);
+    });
+
+    expect(listenerSpy).toBeCalledTimes(1);
+  });
+
+  it('should not the listener when clicked on the target', async () => {
+    const target = document.createElement('div');
+    const mockHandler = jest.fn();
+
+    await runComponentSetup(
+      () => {
+        useClickedOutside(target, mockHandler);
+      },
+      () => {
+        target.dispatchEvent(new MouseEvent('click'));
+      },
+    );
+
+    expect(mockHandler).toBeCalledTimes(0);
+  });
+
+  it('should call the listener when clicked clicked on the body', async () => {
+    const target = document.createElement('div');
+    const mockHandler = jest.fn();
+
+    await runComponentSetup(
+      () => {
+        useClickedOutside(target, mockHandler);
+      },
+      () => {
+        document.body.dispatchEvent(new MouseEvent('click'));
+      },
+    );
+
+    expect(mockHandler).toBeCalledTimes(1);
+  });
+
+  it('should forward the options', async () => {
+    const target = document.createElement('div');
+    const listenerSpy = jest.spyOn(document, 'addEventListener');
+    const mockHandler = jest.fn();
+
+    await runComponentSetup(
+      () => {
+        useClickedOutside(target, mockHandler, { capture: false, passive: false });
+      },
+      () => {
+        document.dispatchEvent(new MouseEvent('click'));
+      },
+    );
+
+    expect(listenerSpy.mock.calls[0][2]).toEqual({ capture: false, passive: false });
+  });
+
+  it('should work with an element Ref', async () => {
+    const { ref } = createMockElementRef();
+
+    const mockHandler = jest.fn();
+
+    await runComponentSetup(
+      () => {
+        useClickedOutside(ref, mockHandler);
+      },
+      () => {
+        document.body.dispatchEvent(new MouseEvent('click'));
+      },
+    );
+
+    expect(mockHandler).toBeCalledTimes(1);
+  });
+
+  it('should work with a component Ref', async () => {
+    const { ref } = createMockComponentRef();
+
+    const mockHandler = jest.fn();
+
+    await runComponentSetup(
+      () => {
+        useClickedOutside(ref, mockHandler);
+      },
+      () => {
+        document.body.dispatchEvent(new MouseEvent('click'));
+      },
+    );
+
+    expect(mockHandler).toBeCalledTimes(1);
+  });
+});
---
+++ src/useClickedOutside/useClickedOutside.ts
@@ -0,0 +1,41 @@
+import type { RefElementType } from '@muban/muban';
+import { useEventListener } from '../useEventListener/useEventListener';
+import type { DomElementOrRef, DomElementsOrRefCollection } from '../utils/util.types';
+import { getElements } from '../utils/getElements';
+
+/**
+ * A small wrapper method to easily check if a user clicked inside a HTMLElement.
+ *
+ * @param target - The target there the user clicked.
+ * @param container - The container where to check against.
+ */
+export function isClickedInside(target: EventTarget, container: RefElementType): boolean {
+  return container ? container.contains(target as HTMLElement) : false;
+}
+
+/**
+ * Will add an event listener to the body and will fire a callback if the user clicks
+ * outside the provided container.
+ *
+ * @param container - The element or elements used to check if the user clicked outside.
+ * @param callback - The callback that will be triggered if the user clicks outside the container.
+ * @param options The options passed to `addEventListener`
+ */
+export function useClickedOutside<T extends RefElementType = HTMLElement>(
+  container: DomElementOrRef<T> | DomElementsOrRefCollection<T>,
+  callback: (event: MouseEvent) => void,
+  options?: { capture?: boolean; passive?: boolean },
+): void {
+  const elements = getElements<T>(container);
+
+  function onDocumentClick(event: MouseEvent) {
+    const { target } = event;
+    const match = target && elements.find((element) => isClickedInside(target, element));
+
+    if (!match) {
+      callback(event);
+    }
+  }
+
+  useEventListener(document, 'click', onDocumentClick, options);
+}
