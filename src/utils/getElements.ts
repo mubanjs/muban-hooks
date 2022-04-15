3cdd90266cae709b562d6131bb1203a56d992271
Lars van Braam <lars@mediamonks.com>
2022-04-15 10:37:54 +0200

    Update the getElements util to support the new muban types and all types of Refs

--- src/utils/getElements.ts
+++ src/utils/getElements.ts
@@ -1,17 +1,29 @@
-import type { DomElementsOrRefCollection } from './util.types';
+import type { RefElementType } from '@muban/muban';
+import type { DomElementOrRef, DomElementsOrRefCollection } from './util.types';
+import { getElement } from './getElement';
 
 /**
  * This helper method can be used to standardize the converting of Muban refs to HTMLElements,
  * this can be very useful when creating utils that support Refs and HTMLElements as the source.
  *
- * Similar to the `getElement` but then applicable to ref collections.
+ * Similar to the `getElement` but it also supports collections and will always return an ArrayP.
  */
-export function getElements(source: DomElementsOrRefCollection): ReadonlyArray<HTMLElement> {
+export function getElements<T extends RefElementType = HTMLElement>(
+  source: DomElementsOrRefCollection<T> | DomElementOrRef<T>,
+): ReadonlyArray<T> {
   if (Array.isArray(source)) return source;
+
+  if (
+    source instanceof HTMLElement ||
+    source instanceof SVGElement ||
+    source.type === 'element' ||
+    source.type === 'component'
+  )
+    return [getElement(source as DomElementOrRef<T>)].filter(Boolean) as Array<T>;
 
   if (source.type === 'collection') return source.getElements();
   if (source.type === 'componentCollection')
-    return source.getComponents().map(({ element }) => element);
+    return source.getComponents().map(({ element }) => element as T);
 
   return [];
 }
